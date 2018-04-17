3a5e708f5080cd3bb4f417a15e5e0401ad43e90f
beginor <beginor@qq.com>
2018-04-17 14:31:21 +0800

    init a map

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,62 @@
+import { loadScript, loadModules } from 'esri-loader';
+
 export class App {
 
-    constructor() { }
+    private view: __esri.SceneView;
 
-    run() {
-        console.log('Hello, Rollup!');
+    constructor(private container: HTMLDivElement) { }
+
+    public run() {
+        this.init().then(() => {
+            console.log('app init');
+        }).catch(ex => {
+            console.error(ex);
+        });
     }
 
-}
-// this function will be treeshaked by rollup
-export function test() {
-    console.log('test');
+    private async init(): Promise<void> {
+        await this.initArcGisJsApi();
+        await this.initMapView();
+    }
+
+    private async initMapView(): Promise<void> {
+        const [Map, SceneView] = await loadModules([
+            'esri/Map',
+            'esri/views/SceneView'
+        ]);
+        const mapProps: __esri.MapProperties = {
+            basemap: 'satellite',
+            ground: 'world-elevation'
+        };
+        const map = new Map(mapProps);
+        const viewProps: __esri.SceneViewProperties = {
+            container: this.container,
+            map: map,
+            zoom: 7,
+            center: {
+                longitude: 113.2,
+                latitude: 23.4
+            },
+            viewingMode: 'local'
+        };
+        this.view = new SceneView(viewProps);
+        this.view.when(() => {
+            console.log('SceneView inited!')
+        });
+    }
+
+    private async initArcGisJsApi() {
+        loadScript({
+            url: 'https://js.arcgis.com/4.6/init.js',
+            css: 'https://js.arcgis.com/4.6/esri/css/main.css',
+            dojoConfig: {
+                async: true,
+                locale: 'zh-cn'
+            }
+        });
+        const [config] = await loadModules(['esri/config']);
+        config.request.corsEnabledServers.push('app.gdep.gov.cn');
+    }
+
+
 }
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import { App } from './app/app'
 
 import './main.css';
 
-var app = new App();
+var app = new App(document.getElementById('map') as HTMLDivElement);
 app.run();
