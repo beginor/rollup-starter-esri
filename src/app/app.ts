d9ef503c830d1b6ca58519d53a568d559bfc5281
beginor <beginor@qq.com>
2019-01-02 15:52:09 +0800

    refact

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,8 @@
-import { loadScript, loadModules } from 'esri-loader';
+import * as esri from 'esri-service';
 
 export class App {
 
-    private view: __esri.SceneView;
+    private sceneView: __esri.SceneView;
 
     constructor(private container: HTMLDivElement) { }
 
@@ -15,50 +15,39 @@ export class App {
     }
 
     private async init(): Promise<void> {
-        await this.getRequestToken();
-        await this.initArcGisJsApi();
         await this.initMapView();
-    }
-
-    private getRequestToken(): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            const request = new XMLHttpRequest();
-            request.withCredentials = true;
-            request.onreadystatechange = (evt) => {
-                if (request.readyState === 4) {
-                    if (request.status === 200) {
-                        resolve();
-                    }
-                }
-            };
-            request.open('GET', 'https://gishub.gdepb.gov.cn/rest/request-token');
-            request.send();
-        });
+        // add your code here.
     }
 
     private async initMapView(): Promise<void> {
-        const [Map, SceneView, ElevationLayer] = await loadModules([
-            'esri/Map',
-            'esri/views/SceneView',
-            'esri/layers/ElevationLayer'
-        ]);
-        const groundProps: __esri.GroundProperties = {
-            surfaceColor: 'blue',
-            layers: [
-                // new ElevationLayer({
-                //     url: 'https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer'
-                // }),
-                new ElevationLayer({
-                    url: 'http://docker7.gdepb.gov.cn/arcgis/rest/services/BaseMap/WGS84_DEM/ImageServer'
-                })
-            ]
-        };
-        const mapProps: __esri.MapProperties = {
+        const map = await esri.createWebScene({
             basemap: 'satellite',
-            ground: groundProps
-        };
-        const map = new Map(mapProps);
-        const viewProps: __esri.SceneViewProperties = {
+            ground: {
+                surfaceColor: 'blue',
+                layers: [
+                    {
+                        type: 'elevation',
+                        url: 'http://docker7.gdepb.gov.cn/arcgis/rest/services/BaseMap/WGS84_DEM/ImageServer'
+                    }
+                ]
+            },
+            layers: [
+                {
+                    type: 'feature',
+                    url: 'https://gishub.gdepb.gov.cn/arcgis/rest/services/mapservices/wgs84_gd_sar_prov_simplify/MapServer/0',
+                    opacity: 0.7,
+                    renderer: {
+                        type: 'simple',
+                        symbol: {
+                            type: 'simple-line',
+                            color: 'red',
+                            width: 2
+                        }
+                    }
+                }
+            ]
+        } as any);
+        this.sceneView = await esri.createSceneView({
             container: this.container,
             map: map,
             zoom: 7,
@@ -67,38 +56,10 @@ export class App {
                 latitude: 23.4
             },
             viewingMode: 'global'
-        };
-        this.view = new SceneView(viewProps);
-        this.view.when(() => {
-            console.log('SceneView inited!');
-            // this.addFeatureLayer();
         });
-    }
-
-    private async initArcGisJsApi(): Promise<void> {
-        const baseUrl = 'https://app.gdep.gov.cn/arcgis-js-api/library/4.9';
-        loadScript({
-            url: baseUrl + '/init.js',
-            css: baseUrl + '/esri/css/main.css',
-            dojoConfig: {
-                async: true,
-                locale: 'zh-cn'
-            }
-        });
-        const [config] = await loadModules(['esri/config']);
-        config.request.trustedServers.push('app.gdep.gov.cn')
-    }
-
-    private async addFeatureLayer(): Promise<void> {
-        const [FeatureLayer] = await loadModules(['esri/layers/FeatureLayer']);
-        const props: __esri.FeatureLayerProperties = {
-            url: 'https://gishub.gdepb.gov.cn/arcgis/rest/services/mapservices/wgs84_gd_sar_prov_simplify/MapServer/0',
-            outFields: ['*'],
-            popupEnabled: true,
-            opacity: 0.3
-        };
-        const layer = new FeatureLayer(props);
-        this.view.map.layers.add(layer);
+        window['_sceneView'] = this.sceneView;
+        await this.sceneView.when();
+        console.log('SceneView inited!');
     }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { loadScript, loadModules } from 'esri-loader';
+
 import { App } from './app/app'
 
 import './main.css';
@@ -5,6 +7,38 @@ import './main.css';
 let app: App;
 
 document.addEventListener('DOMContentLoaded', e => {
-    app = new App(document.getElementById('map') as HTMLDivElement);
-    app.run();
+    // get request token first
+    getRequestToken().then(() => {
+        // then init arcgis js api
+        return initArcGisJsApi();
+    }).then(() => {
+        // and then run app
+        app = new App(document.getElementById('map') as HTMLDivElement);
+        app.run();
+    });
+
 });
+
+async function getRequestToken(): Promise<void> {
+    const res = await fetch(
+        'https://gishub.gdepb.gov.cn/rest/request-token',
+        { method: 'get', credentials: 'include' }
+    );
+    if (!res.ok) {
+        throw new Error('Can not get request token from gishub !');
+    }
+}
+
+async function initArcGisJsApi(): Promise<void> {
+    const baseUrl = 'https://app.gdep.gov.cn/arcgis-js-api/library/4.10';
+    loadScript({
+        url: baseUrl + '/init.js',
+        css: baseUrl + '/esri/css/main.css',
+        dojoConfig: {
+            async: true,
+            locale: 'zh-cn'
+        }
+    });
+    const [config] = await loadModules(['esri/config']);
+    config.request.trustedServers.push('gishub.gdepb.gov.cn')
+}
