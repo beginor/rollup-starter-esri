b884fa085dac9fe9074444e892f010c6e0d63ec1
beginor <beginor@qq.com>
2021-02-26 10:45:39 +0800

    update map

--- src/app/app.js
+++ src/app/app.js
@@ -1,16 +1,65 @@
+import { loadScript } from 'esri-loader';
+import * as arcgis from 'esri-service';
+
 export class App {
 
-    /** @type {string} app title */
-    title;
+    /** @private @type {__esri.SceneView} */
+    sceneView = undefined;
+    /** @private @type {HTMLDivElement} */
+    container = undefined;
 
-    constructor() { }
+    constructor(/** @type {HTMLDivElement} */ container) {
+        this.container = container;
+    }
 
     /**
      * run the app.
      */
     run() {
-        this.title
-        console.log('Hello, Rollup!');
+        this.init().catch(ex => console.error(ex));
+    }
+
+    /** @private */
+    async init() {
+        await this.initArcGISApi()
+        await this.initMapView();
+    }
+
+    /** @private */
+    async initArcGISApi() {
+        const baseUrl = 'https://app.gdeei.cn/arcgis-js-api/library/4.17';
+        await loadScript({
+            url: `${baseUrl}/init.js`,
+            css: `${baseUrl}/esri/css/main.css`,
+            dojoConfig: {
+                async: true,
+                locale: 'zh-cn',
+                has: {
+                    'esri-native-promise': true
+                }
+            }
+        });
+    }
+
+    /** @private */
+    async initMapView() {
+        const map = await arcgis.createWebScene({
+            basemap: 'satellite',
+            ground: 'world-elevation',
+        });
+        this.sceneView = await arcgis.createSceneView({
+            container: this.container,
+            map: map,
+            zoom: 7,
+            center: {
+                longitude: 113.2,
+                latitude: 23.4
+            },
+            viewingMode: 'global'
+        });
+        window['_sceneView'] = this.sceneView;
+        await this.sceneView.when();
+        console.log('SceneView inited!');
     }
 
 }
